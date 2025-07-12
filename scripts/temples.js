5c07eb86a26b09c08349f7b9bfaccfd03c238dcd
efficodex <alexandraruizfernandez01@gmail.com>
2025-07-11 21:04:45 -0500

    web temples lds

---
+++ scripts/temples.js
@@ -0,0 +1,16 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const year = document.querySelector("#currentYear");
+let lastModified1 = document.querySelector("#lastModified")
+
+//the objects
+const today = new Date();
+let oLastModif = new Date(document.lastModified);
+year.innerHTML += `${today.getFullYear()} Alexandra Fernandez - Perú`;
+lastModified1.innerHTML += oLastModif;
