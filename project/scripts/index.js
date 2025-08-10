dd3711f97c14fcede68091aaf9f10934d8a9a6cd
efficodex <alexandraruizfernandez01@gmail.com>
2025-08-10 12:59:35 -0500

    emprendimientos

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -42,3 +42,25 @@ emprendimientosAleatorios.forEach(emp => {
     logosContainer.appendChild(circle);
 });
 
+// Contenedor
+const contenedor = document.getElementById('lista-emprendimientos');
+
+// Crear tarjetas
+emprendimientos.forEach(emp => {
+    const card = document.createElement('div');
+    card.classList.add('card-emprendimiento');
+
+    card.innerHTML = `
+        <img src="${emp.imagen}" alt="${emp.nombre}">
+        <div class="card-content">
+            <h2>${emp.nombre}</h2>
+            <p>${emp.descripcion}</p>
+            <a href="${emp.redes.facebook}" target="_blank">Ver en Facebook</a>
+        </div>
+    `;
+
+    contenedor.appendChild(card);
+});
+
+
+
